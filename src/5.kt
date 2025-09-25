7e2fdb613680e2ea0cd8728009eeede561ae0f3d
10230280 <rdxzz@iughv>
2025-09-25 11:17:48 +0300

    сделал 5

--- src/5.kt
+++ src/5.kt
@@ -1,3 +1,18 @@
-fun main(){
-
+fun main() {
+    println("Введите слова через запятую:")
+    val input = readLine() ?: ""
+    val words = input.split(",").map { it.trim() }
+    val groups = mutableMapOf<String, MutableList<String>>()
+    for (word in words) {
+        val key = word.toCharArray().sorted().joinToString("")
+        if (groups.containsKey(key)) {
+            groups[key]?.add(word)
+        } else {
+            groups[key] = mutableListOf(word)
+        }
+    }
+    println("Результат группировки:")
+    for (group in groups.values) {
+        println(group)
+    }
 }
